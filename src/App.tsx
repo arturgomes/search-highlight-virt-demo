ea1c5df7e98a9a47a88fb3975b37c7a3cae594d0
Artur Oliveira Gomes <12086+arturgomes@users.noreply.github.com>
2026-05-20 08:18:14 -0300

    feat: App entry composing Toolbar + BlogViewer

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Toolbar } from './components/Toolbar';
+import { BlogViewer } from './components/BlogViewer';
+
+export default function App() {
+	return (
+		<div className="app">
+			<Toolbar />
+			<BlogViewer />
+		</div>
+	);
+}
